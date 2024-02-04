0afac9f5e771f7bcf16b4334ff23181b72689402
Viacheslav Blazhkov <viacheslav.b@droam.com>
2024-02-04 14:19:03 +0200

    File separating

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,33 @@
-let popup = document.getElementById("call-me-popup"),
-    callMeBtn = document.getElementById("call-me-tab"),
-    span = document.getElementsByClassName("close")[0]
-;
-callMeBtn.onclick = function () {
-    popup.style.display = "block";
-}
-span.onclick = function () {
-    popup.style.display = "none";
-}
-window.onclick = function (event) {
-    if (event.target === popup) {
-        popup.style.display = "none";
-    }
-}
-
-function register() {
-    let popup = document.getElementById('call-me-popup'),
-        fullName = document.getElementById("js-full-name").value,
-        phoneNumber = document.getElementById("js-phone-number").value,
-        email = document.getElementById("js-email").value
+$(document).ready(function () {
+    const
+        $content = $('#content'),
+        $header = $('#header'),
+        $headerTabs = $header.find('.header-tab')
     ;
-}
+    $content.load('/views/content/home.html');
+    Array.from($headerTabs).forEach(function (tab) {
+        tab.addEventListener('click', function () {
+            switch (tab.id) {
+                case 'main-tab':
+                case 'logo':
+                    $content.load('/views/content/home.html')
+                    break;
+                case 'photo-sessions-tab':
+                    $content.load('/views/content/photo-sessions.html')
+                    break;
+                case 'calendar-tab':
+                    $content.load('/views/content/calendar.html')
+                    break;
+                case 'prices-tab':
+                    $content.load('/views/content/prices.html')
+                    break;
+                case 'location-tab':
+                    $content.load('/views/content/location.html')
+                    break;
+                case 'call-me-tab':
+                    $content.load('/views/content/contacts.html')
+                    break;
+            }
+        });
+    });
+});
